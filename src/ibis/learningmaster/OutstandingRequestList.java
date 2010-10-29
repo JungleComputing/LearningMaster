0bcf80bc602fbd06288b4ed481e06439df5a1164
Kees van Reeuwijk <keesvr@xs4all.nl>
2010-10-29 11:20:29 +0000

    Equalizing interfaces with the maestro ones.

--- src/ibis/learningmaster/OutstandingRequestList.java
+++ src/ibis/learningmaster/OutstandingRequestList.java
@@ -5,61 +5,62 @@ import ibis.ipl.IbisIdentifier;
 import java.util.LinkedList;
 
 class OutstandingRequestList {
-	private final LinkedList<OutstandingRequest> requests = new LinkedList<OutstandingRequestList.OutstandingRequest>();
+    private final LinkedList<OutstandingRequest> requests = new LinkedList<OutstandingRequestList.OutstandingRequest>();
 
-	private static class OutstandingRequest {
-		private final int id;
-		private final IbisIdentifier worker;
+    private static class OutstandingRequest {
+        private final int id;
+        private final IbisIdentifier worker;
 
-		public OutstandingRequest(final IbisIdentifier worker, final int id) {
-			this.id = id;
-			this.worker = worker;
-		}
+        public OutstandingRequest(final IbisIdentifier worker, final int id) {
+            this.id = id;
+            this.worker = worker;
+        }
 
-		@Override
-		public String toString() {
-			return "workId=" + id + " worker=" + worker;
-		}
-	}
+        @Override
+        public String toString() {
+            return "workId=" + id + " worker=" + worker;
+        }
+    }
 
-	void add(final IbisIdentifier worker, final int id){
-		final OutstandingRequest rq = new OutstandingRequest(worker, id);
-		requests.add(rq);
-	}
+    void add(final IbisIdentifier worker, final int id) {
+        final OutstandingRequest rq = new OutstandingRequest(worker, id);
+        requests.add(rq);
+    }
 
-	void dumpState() {
-		Globals.log.reportProgress("There are " + requests.size()
-				+ " outstanding requests:");
-		for (final OutstandingRequest r : requests) {
-			Globals.log.reportProgress(" Request " + r);
-		}
-	}
+    void dumpState() {
+        Globals.log.reportProgress("There are " + requests.size()
+                + " outstanding requests:");
+        for (final OutstandingRequest r : requests) {
+            Globals.log.reportProgress(" Request " + r);
+        }
+    }
 
-	@SuppressWarnings("synthetic-access")
-	void removePeer(final IbisIdentifier peer, final Scheduler scheduler) {
-		for (final OutstandingRequest r : requests) {
-			if (peer.equals(r.worker)) {
-				requests.remove(r);
-				scheduler.returnTask(r.id);
-				Globals.log.reportProgress("Returning request " + r
-						+ " to scheduler, since this worker has gone");
-			}
-		}
-	}
+    @SuppressWarnings("synthetic-access")
+    void removePeer(final IbisIdentifier peer, final Scheduler scheduler) {
+        for (final OutstandingRequest r : requests) {
+            if (peer.equals(r.worker)) {
+                requests.remove(r);
+                scheduler.returnTask(r.id);
+                Globals.log.reportProgress("Returning request " + r
+                        + " to scheduler, since this worker has gone");
+            }
+        }
+    }
 
-	boolean isEmpty() {
-		return requests.isEmpty();
-	}
+    boolean isEmpty() {
+        return requests.isEmpty();
+    }
 
-	boolean removeTask(final int id) {
-		for (final OutstandingRequest r : requests) {
-			if (r.id == id) {
-				Globals.log.reportProgress("Returning request " + r
-						+ " to scheduler");
-				requests.remove(r);
-				return true;
-			}
-		}
-		return false;
-	}
+    @SuppressWarnings("synthetic-access")
+    boolean removeTask(final int id) {
+        for (final OutstandingRequest r : requests) {
+            if (r.id == id) {
+                Globals.log.reportProgress("Returning request " + r
+                        + " to scheduler");
+                requests.remove(r);
+                return true;
+            }
+        }
+        return false;
+    }
 }
