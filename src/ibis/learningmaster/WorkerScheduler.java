60551bb5468909e58a7467924c571643650a12f7
Kees van Reeuwijk <keesvr@xs4all.nl>
2010-11-04 18:41:45 +0000

    Refined stochastic master.

--- src/ibis/learningmaster/WorkerScheduler.java
+++ src/ibis/learningmaster/WorkerScheduler.java
@@ -9,67 +9,64 @@ import java.io.PrintStream;
  * 
  */
 class WorkerScheduler implements Scheduler {
-	private boolean masterHasGone = false;
-	private final IbisIdentifier master;
+    private boolean masterHasGone = false;
+    private final IbisIdentifier master;
 
-	WorkerScheduler(final IbisIdentifier master) {
-		this.master = master;
-	}
+    WorkerScheduler(final IbisIdentifier master) {
+        this.master = master;
+    }
 
-	@Override
-	public void shutdown() {
-	}
+    @Override
+    public void shutdown() {
+    }
 
-	@Override
-	public void removePeer(final IbisIdentifier peer) {
-		if (peer.equals(master)) {
-			masterHasGone = true;
-		}
-	}
+    @Override
+    public void removePeer(final IbisIdentifier peer) {
+        if (peer.equals(master)) {
+            masterHasGone = true;
+        }
+    }
 
-	@Override
-	public void dumpState() {
-		Globals.log.reportProgress("master=" + master + " masterHasGone="
-				+ masterHasGone);
-	}
+    @Override
+    public void dumpState() {
+        Globals.log.reportProgress("master=" + master + " masterHasGone="
+                + masterHasGone);
+    }
 
-	@Override
-	public void peerHasJoined(final IbisIdentifier source) {
-		// TODO Auto-generated method stub
+    @Override
+    public void peerHasJoined(final IbisIdentifier source) {
+        // Ignore
+    }
 
-	}
+    @Override
+    public void registerCompletedTask(final int task) {
+        Globals.log
+                .reportInternalError("Someone tried to register a completed task to the worker scheduler");
+    }
 
-	@Override
-	public void registerCompletedTask(final int task) {
-		Globals.log
-		.reportInternalError("Someone tried to register a completed task to the worker scheduler");
-	}
+    @Override
+    public void printStatistics(final PrintStream printStream) {
+    }
 
-	@Override
-	public void printStatistics(final PrintStream printStream) {
-		// TODO Auto-generated method stub
+    @Override
+    public boolean shouldStop() {
+        return masterHasGone;
+    }
 
-	}
+    @Override
+    public void returnTask(final int id) {
+        Globals.log
+                .reportInternalError("Someone tried to return a task to the worker scheduler");
+    }
 
-	@Override
-	public boolean shouldStop() {
-		return masterHasGone;
-	}
+    @Override
+    public boolean maintainOutstandingRequests(final Transmitter transmitter) {
+        return false;
+    }
 
-	@Override
-	public void returnTask(final int id) {
-		Globals.log
-		.reportInternalError("Someone tried to return a task to the worker scheduler");
-	}
-
-	@Override
-	public boolean maintainOutstandingRequests( final Transmitter transmitter) {
-		return false;
-	}
-
-	@Override
-	public boolean requestsToSubmit() {
-		return false;
-	}
+    @Override
+    public boolean requestsToSubmit() {
+        return false;
+    }
 
 }
